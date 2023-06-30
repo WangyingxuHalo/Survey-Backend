5d5bbc6cb7f7ed6fe7c81fb5ad36463b69bf46df
yxw <yw473@duke.edu>
2023-06-29 22:28:16 -0400

    chore: fix count not correct bug

--- api/answer.js
+++ api/answer.js
@@ -2,6 +2,7 @@ const Router = require("koa-router")
 const router = new Router();
 
 const { Answer } = require("../models/answer")
+const { Question } = require("../models/question")
 
 router.post('/api/answer', async (ctx) => {
     const body = ctx.request.body
@@ -12,6 +13,7 @@ router.post('/api/answer', async (ctx) => {
         const answerToCreate = { fe_id: componentId, value, question_id: questionId, identifier }
         await Answer.create(answerToCreate)
     })
+    await Question.increment('answerCount', { where: { id: questionId}})
 
     ctx.body = {
         errno: 0
